a0e05f604fcd69e28dab61e0dc0e39fc43671524
doozerovka-svg <doozerovka@gmail.com>
2026-05-25 09:18:24 +0300

    Fix syntax error in Compose UI test manifest dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -93,5 +93,5 @@ dependencies {
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test-manifest)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
